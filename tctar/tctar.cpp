864c9d61ff72d7572b3051770e2b1a4c23fd9fc4
Ming Chen <v.mingchen@gmail.com>
2016-09-20 11:06:22 -0400

    format code with clangformat
    No coment change. Only formatting.
    
    Signed-off-by: Ming Chen <v.mingchen@gmail.com>

--- tctar/tctar.cpp
+++ tctar/tctar.cpp
@@ -11,217 +11,238 @@
 
 #define DEFAULT_LOG_FILE "/tmp/libarchive-tctar.log"
 
-struct write_cbarg {
-    struct tc_iovec output_file;
-    std::vector<struct tc_iovec> writes;
-};
-
-ssize_t tc_archive_write(struct archive *a, void *client_data, const void *buffer, size_t length)
+struct write_cbarg
 {
-    struct write_cbarg *cbarg = (struct write_cbarg*) client_data;
-    struct tc_iovec *output_file = &cbarg->output_file;
-
-    char *data = (char*) malloc(length);
-    memcpy(data, buffer, length);
-
-    output_file->data = data;
-    output_file->length = length;
-
-    cbarg->writes.push_back(*output_file);
-
-    if (cbarg->writes.size() >= 255) {
-        tc_res res = tc_writev(cbarg->writes.data(), cbarg->writes.size(), false);
-        if (!tc_okay(res)) {
-            printf("writev: %s\n", strerror(res.err_no));
-            return -1;
-        }
-        for (auto& write : cbarg->writes) {
-            free(write.data);
-        }
-        cbarg->writes.clear();
-    }
-
-    output_file->offset += length;
-    return length;
-}
-
-int tc_archive_close(struct archive *a, void *client_data) {
-    struct write_cbarg *cbarg = (struct write_cbarg*) client_data;
-
-    if (cbarg->writes.size() > 0) {
-        tc_res res = tc_writev(cbarg->writes.data(), cbarg->writes.size(), false);
-        if (!tc_okay(res)) {
-            printf("writev: %s\n", strerror(res.err_no));
-            return ARCHIVE_FATAL;
-        }
-        cbarg->writes.clear();
-    }
-    return ARCHIVE_OK;
-}
-
-void deinit(int status, void *context) {
-    tc_deinit(context);
-}
-
-struct cbarg {
-    struct archive *a;
-    std::vector<struct archive_entry *> files;
-    std::vector<struct archive_entry *> symlinks;
+	struct tc_iovec output_file;
+	std::vector<struct tc_iovec> writes;
 };
 
-tc_res read_files_and_write_to_archive(struct archive *a, std::vector<struct archive_entry *>& files,
-        std::vector<struct archive_entry *>& symlinks, int min_count) {
+ssize_t tc_archive_write(struct archive *a, void *client_data,
+			 const void *buffer, size_t length)
+{
+	struct write_cbarg *cbarg = (struct write_cbarg *)client_data;
+	struct tc_iovec *output_file = &cbarg->output_file;
 
-    tc_res res = {0, 0};
-    const char **path_names;
-    struct tc_iovec *reads;
+	char *data = (char *)malloc(length);
+	memcpy(data, buffer, length);
 
-    if (files.size() >= min_count) {
-        reads = (struct tc_iovec *) malloc(sizeof(struct tc_iovec) * files.size());
-        for (int i = 0; i < files.size(); i++) {
-            reads[i].file = tc_file_from_path(archive_entry_pathname(files[i]));
-            reads[i].offset = 0;
-            reads[i].length = archive_entry_size(files[i]);
-            reads[i].is_creation = false;
-            char *buf = (char *) malloc(reads[i].length);
-            reads[i].data = buf;
-        }
-        res = tc_readv(reads, files.size(), false);
-        if (!tc_okay(res)) {
-            printf("readv: %s\n", strerror(res.err_no));
-            return res;
-        }
-        for (int i = 0; i < files.size(); i++) {
-            archive_write_header(a, files[i]);
-            ssize_t w = archive_write_data(a, reads[i].data, reads[i].length);
-            if (w != reads[i].length) {
-                printf("could not write all data to archive\n");
-            }
-            archive_entry_free(files[i]);
-            free(reads[i].data);
-        }
-        free(reads);
-        files.clear();
-    }
-    if (symlinks.size() >= min_count) {
-        path_names = (const char **) malloc(sizeof(const char *) * symlinks.size());
-        char **bufs = (char **) malloc(sizeof(char *) * symlinks.size());
-        size_t *bufsizes = (size_t *) malloc(sizeof(size_t) * symlinks.size());
-        for (int i = 0; i < symlinks.size(); i++) {
-            path_names[i] = archive_entry_pathname(symlinks[i]);
-            bufs[i] = (char *) malloc(sizeof(char) * PATH_MAX);
-            bufsizes[i] = PATH_MAX;
-        }
-        res = tc_readlinkv(path_names, bufs, bufsizes, symlinks.size(), false);
-        if (!tc_okay(res)) {
-            printf("readlinkv: %s\n", strerror(res.err_no));
-            return res;
-        }
-        for (int i = 0; i < symlinks.size(); i++) {
-            archive_entry_set_symlink(symlinks[i], bufs[i]);
-            archive_write_header(a, symlinks[i]);
-            archive_entry_free(symlinks[i]);
-            free(bufs[i]);
-        }
-        free(bufs);
-        free(bufsizes);
-        symlinks.clear();
-    }
-    return res;
+	output_file->data = data;
+	output_file->length = length;
+
+	cbarg->writes.push_back(*output_file);
+
+	if (cbarg->writes.size() >= 255) {
+		tc_res res = tc_writev(cbarg->writes.data(),
+				       cbarg->writes.size(), false);
+		if (!tc_okay(res)) {
+			printf("writev: %s\n", strerror(res.err_no));
+			return -1;
+		}
+		for (auto &write : cbarg->writes) {
+			free(write.data);
+		}
+		cbarg->writes.clear();
+	}
+
+	output_file->offset += length;
+	return length;
 }
 
-bool listdir_callback(const struct tc_attrs *attr, const char *dir, void *arg) {
-    struct cbarg *cbarg = (struct cbarg *) arg;
-    struct archive_entry *entry = archive_entry_new();
+int tc_archive_close(struct archive *a, void *client_data)
+{
+	struct write_cbarg *cbarg = (struct write_cbarg *)client_data;
 
-    archive_entry_copy_pathname(entry, attr->file.path);
-    archive_entry_set_size(entry, attr->size);
-    archive_entry_set_mode(entry, attr->mode);
-
-    if (S_ISDIR(attr->mode)) {
-        archive_write_header(cbarg->a, entry);
-        archive_entry_free(entry);
-    } else if (S_ISREG(attr->mode)) {
-        cbarg->files.push_back(entry);
-    } else if (S_ISLNK(attr->mode)) {
-        cbarg->symlinks.push_back(entry);
-    } else {
-        printf("unsupported file type\n");
-    }
-
-    if (!tc_okay(read_files_and_write_to_archive(cbarg->a, cbarg->files, cbarg->symlinks, 255))) {
-        printf("read_files_and_write_to_archive failed\n");
-        return false;
-    }
-
-    return true;
+	if (cbarg->writes.size() > 0) {
+		tc_res res = tc_writev(cbarg->writes.data(),
+				       cbarg->writes.size(), false);
+		if (!tc_okay(res)) {
+			printf("writev: %s\n", strerror(res.err_no));
+			return ARCHIVE_FATAL;
+		}
+		cbarg->writes.clear();
+	}
+	return ARCHIVE_OK;
 }
 
+void deinit(int status, void *context)
+{
+	tc_deinit(context);
+}
 
-int main(int argc, char **argv) {
-    char exe_path[PATH_MAX];
-    char tc_config_path[PATH_MAX];
-    void *context;
-    struct archive *a;
-    int r;
-    tc_res res;
-    struct cbarg *cbarg = (struct cbarg *) malloc(sizeof(struct cbarg));
-    struct write_cbarg *write_cbarg = (struct write_cbarg *) malloc(sizeof(struct write_cbarg));
+struct cbarg
+{
+	struct archive *a;
+	std::vector<struct archive_entry *> files;
+	std::vector<struct archive_entry *> symlinks;
+};
 
-    readlink("/proc/self/exe", exe_path, PATH_MAX);
-    snprintf(tc_config_path, PATH_MAX,
-                 "%s/../../../../config/tc.ganesha.conf", dirname(exe_path));
-    fprintf (stderr, "using config file: %s\n", tc_config_path);
+tc_res read_files_and_write_to_archive(
+    struct archive *a, std::vector<struct archive_entry *> &files,
+    std::vector<struct archive_entry *> &symlinks, int min_count)
+{
 
-    context = tc_init (tc_config_path, DEFAULT_LOG_FILE, 77);
-    if (!context)
-    {
-        printf("initializing tc failed\n");
-        return 1;
-    }
-    if (on_exit(deinit, (void*)context) != 0) {
-        fprintf(stderr, "warning: failed to register tc_deinit() on exit");
-    }
+	tc_res res = { 0, 0 };
+	const char **path_names;
+	struct tc_iovec *reads;
 
-    a = archive_write_new();
-    archive_write_set_format_ustar(a);
-    if (strcmp(argv[1], "--no-compress") == 0) {
-        argv++;
-    } else {
-        archive_write_add_filter_xz(a);
-    }
+	if (files.size() >= min_count) {
+		reads = (struct tc_iovec *)malloc(sizeof(struct tc_iovec) *
+						  files.size());
+		for (int i = 0; i < files.size(); i++) {
+			reads[i].file =
+			    tc_file_from_path(archive_entry_pathname(files[i]));
+			reads[i].offset = 0;
+			reads[i].length = archive_entry_size(files[i]);
+			reads[i].is_creation = false;
+			char *buf = (char *)malloc(reads[i].length);
+			reads[i].data = buf;
+		}
+		res = tc_readv(reads, files.size(), false);
+		if (!tc_okay(res)) {
+			printf("readv: %s\n", strerror(res.err_no));
+			return res;
+		}
+		for (int i = 0; i < files.size(); i++) {
+			archive_write_header(a, files[i]);
+			ssize_t w = archive_write_data(a, reads[i].data,
+						       reads[i].length);
+			if (w != reads[i].length) {
+				printf("could not write all data to archive\n");
+			}
+			archive_entry_free(files[i]);
+			free(reads[i].data);
+		}
+		free(reads);
+		files.clear();
+	}
+	if (symlinks.size() >= min_count) {
+		path_names = (const char **)malloc(sizeof(const char *) *
+						   symlinks.size());
+		char **bufs = (char **)malloc(sizeof(char *) * symlinks.size());
+		size_t *bufsizes =
+		    (size_t *)malloc(sizeof(size_t) * symlinks.size());
+		for (int i = 0; i < symlinks.size(); i++) {
+			path_names[i] = archive_entry_pathname(symlinks[i]);
+			bufs[i] = (char *)malloc(sizeof(char) * PATH_MAX);
+			bufsizes[i] = PATH_MAX;
+		}
+		res = tc_readlinkv(path_names, bufs, bufsizes, symlinks.size(),
+				   false);
+		if (!tc_okay(res)) {
+			printf("readlinkv: %s\n", strerror(res.err_no));
+			return res;
+		}
+		for (int i = 0; i < symlinks.size(); i++) {
+			archive_entry_set_symlink(symlinks[i], bufs[i]);
+			archive_write_header(a, symlinks[i]);
+			archive_entry_free(symlinks[i]);
+			free(bufs[i]);
+		}
+		free(bufs);
+		free(bufsizes);
+		symlinks.clear();
+	}
+	return res;
+}
 
-    write_cbarg->output_file.file = tc_file_from_path(argv[1]);
-    write_cbarg->output_file.offset = 0;
-    write_cbarg->output_file.is_creation = true;
-    write_cbarg->writes = {};
+bool listdir_callback(const struct tc_attrs *attr, const char *dir, void *arg)
+{
+	struct cbarg *cbarg = (struct cbarg *)arg;
+	struct archive_entry *entry = archive_entry_new();
 
-    r = archive_write_open(a, write_cbarg, NULL, tc_archive_write, tc_archive_close);
-    if (r != ARCHIVE_OK) {
-        printf("error, could not open archive");
-        return 1;
-    }
+	archive_entry_copy_pathname(entry, attr->file.path);
+	archive_entry_set_size(entry, attr->size);
+	archive_entry_set_mode(entry, attr->mode);
 
-    struct tc_attrs_masks listdir_mask = TC_ATTRS_MASK_NONE;
-    listdir_mask.has_mode = true;
-    listdir_mask.has_size = true;
-    cbarg->a = a;
-    cbarg->files = {};
-    cbarg->symlinks = {};
-    res = tc_listdirv((const char **) &argv[2], 1, listdir_mask, 0, true, listdir_callback, cbarg, false);
-    if (!tc_okay(res)) {
-        printf("listdir: %s\n", strerror(res.err_no));
-        return res.err_no;
-    }
-    if (!tc_okay(read_files_and_write_to_archive(cbarg->a, cbarg->files, cbarg->symlinks, 1))) {
-        return 1;
-    }
+	if (S_ISDIR(attr->mode)) {
+		archive_write_header(cbarg->a, entry);
+		archive_entry_free(entry);
+	} else if (S_ISREG(attr->mode)) {
+		cbarg->files.push_back(entry);
+	} else if (S_ISLNK(attr->mode)) {
+		cbarg->symlinks.push_back(entry);
+	} else {
+		printf("unsupported file type\n");
+	}
 
-    r = archive_write_free(a);
-    free(write_cbarg);
-    free(cbarg);
-    if (r != ARCHIVE_OK) {
-        printf("error, could not free archive");
-        return 1;
-    }
+	if (!tc_okay(read_files_and_write_to_archive(cbarg->a, cbarg->files,
+						     cbarg->symlinks, 255))) {
+		printf("read_files_and_write_to_archive failed\n");
+		return false;
+	}
+
+	return true;
+}
+
+int main(int argc, char **argv)
+{
+	char exe_path[PATH_MAX];
+	char tc_config_path[PATH_MAX];
+	void *context;
+	struct archive *a;
+	int r;
+	tc_res res;
+	struct cbarg *cbarg = (struct cbarg *)malloc(sizeof(struct cbarg));
+	struct write_cbarg *write_cbarg =
+	    (struct write_cbarg *)malloc(sizeof(struct write_cbarg));
+
+	readlink("/proc/self/exe", exe_path, PATH_MAX);
+	snprintf(tc_config_path, PATH_MAX,
+		 "%s/../../../../config/tc.ganesha.conf", dirname(exe_path));
+	fprintf(stderr, "using config file: %s\n", tc_config_path);
+
+	context = tc_init(tc_config_path, DEFAULT_LOG_FILE, 77);
+	if (!context) {
+		printf("initializing tc failed\n");
+		return 1;
+	}
+	if (on_exit(deinit, (void *)context) != 0) {
+		fprintf(stderr,
+			"warning: failed to register tc_deinit() on exit");
+	}
+
+	a = archive_write_new();
+	archive_write_set_format_ustar(a);
+	if (strcmp(argv[1], "--no-compress") == 0) {
+		argv++;
+	} else {
+		archive_write_add_filter_xz(a);
+	}
+
+	write_cbarg->output_file.file = tc_file_from_path(argv[1]);
+	write_cbarg->output_file.offset = 0;
+	write_cbarg->output_file.is_creation = true;
+	write_cbarg->writes = {};
+
+	r = archive_write_open(a, write_cbarg, NULL, tc_archive_write,
+			       tc_archive_close);
+	if (r != ARCHIVE_OK) {
+		printf("error, could not open archive");
+		return 1;
+	}
+
+	struct tc_attrs_masks listdir_mask = TC_ATTRS_MASK_NONE;
+	listdir_mask.has_mode = true;
+	listdir_mask.has_size = true;
+	cbarg->a = a;
+	cbarg->files = {};
+	cbarg->symlinks = {};
+	res = tc_listdirv((const char **)&argv[2], 1, listdir_mask, 0, true,
+			  listdir_callback, cbarg, false);
+	if (!tc_okay(res)) {
+		printf("listdir: %s\n", strerror(res.err_no));
+		return res.err_no;
+	}
+	if (!tc_okay(read_files_and_write_to_archive(cbarg->a, cbarg->files,
+						     cbarg->symlinks, 1))) {
+		return 1;
+	}
+
+	r = archive_write_free(a);
+	free(write_cbarg);
+	free(cbarg);
+	if (r != ARCHIVE_OK) {
+		printf("error, could not free archive");
+		return 1;
+	}
 }
--- tctar/tcuntar.cpp
+++ tctar/tcuntar.cpp
@@ -14,184 +14,197 @@
 ssize_t tc_archive_read(struct archive *a, void *client_data, const void **buff)
 {
 
-    struct tc_iovec *input_file = (tc_iovec*) client_data;
+	struct tc_iovec *input_file = (tc_iovec *)client_data;
 
-    tc_res res = tc_readv(input_file, 1, false);
-    if (!tc_okay(res)) {
-        printf("readv: %s\n", strerror(res.err_no));
-        return res.err_no;
-    }
-    *buff = input_file->data;
-    input_file->offset += input_file->length;
-    return input_file->length;
+	tc_res res = tc_readv(input_file, 1, false);
+	if (!tc_okay(res)) {
+		printf("readv: %s\n", strerror(res.err_no));
+		return res.err_no;
+	}
+	*buff = input_file->data;
+	input_file->offset += input_file->length;
+	return input_file->length;
 }
 
-void deinit(int status, void *context) {
-    tc_deinit(context);
+void deinit(int status, void *context)
+{
+	tc_deinit(context);
 }
 
-tc_res performTCIO(std::vector<struct tc_attrs>& directories,
-                 std::vector<struct tc_iovec>& writes,
-                 std::vector<const char *>& symlink_src_paths,
-                 std::vector<const char *>& symlink_dst_paths,
-                 int min_count) {
-    tc_res res = {0, 0};
+tc_res performTCIO(std::vector<struct tc_attrs> &directories,
+		   std::vector<struct tc_iovec> &writes,
+		   std::vector<const char *> &symlink_src_paths,
+		   std::vector<const char *> &symlink_dst_paths, int min_count)
+{
+	tc_res res = { 0, 0 };
 
-    auto make_dirs = [](std::vector<struct tc_attrs>& directories) {
-        tc_res res = {0, 0};
-        if (directories.size() > 0) {
-            res = tc_mkdirv(directories.data(), directories.size(), false);
-            if (!tc_okay(res)) {
-                printf("mkdirv: %s (%s)\n", strerror(res.err_no), directories[res.index].file.path);
-                return res;
-            }
-            for (auto& dir : directories) {
-                free((char*) dir.file.path);
-            }
-            directories.clear();
-        }
-        return res;
-    };
+	auto make_dirs = [](std::vector<struct tc_attrs> &directories) {
+		tc_res res = { 0, 0 };
+		if (directories.size() > 0) {
+			res = tc_mkdirv(directories.data(), directories.size(),
+					false);
+			if (!tc_okay(res)) {
+				printf("mkdirv: %s (%s)\n",
+				       strerror(res.err_no),
+				       directories[res.index].file.path);
+				return res;
+			}
+			for (auto &dir : directories) {
+				free((char *)dir.file.path);
+			}
+			directories.clear();
+		}
+		return res;
+	};
 
-    if (directories.size() >= min_count) {
-        res = make_dirs(directories);
-        if (!tc_okay(res)) {
-            return res;
-        }
-    }
+	if (directories.size() >= min_count) {
+		res = make_dirs(directories);
+		if (!tc_okay(res)) {
+			return res;
+		}
+	}
 
+	if (symlink_src_paths.size() >= min_count) {
+		res = make_dirs(directories);
+		if (!tc_okay(res)) {
+			return res;
+		}
+		res = tc_symlinkv(symlink_src_paths.data(),
+				  symlink_dst_paths.data(),
+				  symlink_src_paths.size(), false);
+		if (!tc_okay(res)) {
+			printf("symlinkv: %s (%s)\n", strerror(res.err_no),
+			       symlink_src_paths[res.index]);
+			return res;
+		}
+		for (auto &s : symlink_src_paths) {
+			free((char *)s);
+		}
+		for (auto &s : symlink_dst_paths) {
+			free((char *)s);
+		}
+		symlink_src_paths.clear();
+		symlink_dst_paths.clear();
+	}
 
-    if (symlink_src_paths.size() >= min_count) {
-        res = make_dirs(directories);
-        if (!tc_okay(res)) {
-            return res;
-        }
-        res = tc_symlinkv(symlink_src_paths.data(), symlink_dst_paths.data(),
-                symlink_src_paths.size(), false);
-        if (!tc_okay(res)) {
-            printf("symlinkv: %s (%s)\n", strerror(res.err_no), symlink_src_paths[res.index]);
-            return res;
-        }
-        for (auto& s : symlink_src_paths) {
-            free((char*)s);
-        }
-        for (auto& s : symlink_dst_paths) {
-            free((char*)s);
-        }
-        symlink_src_paths.clear();
-        symlink_dst_paths.clear();
-    }
+	if (writes.size() >= min_count) {
+		res = make_dirs(directories);
+		if (!tc_okay(res)) {
+			return res;
+		}
+		res = tc_writev(writes.data(), writes.size(), false);
+		if (!tc_okay(res)) {
+			printf("writev: %s\n", strerror(res.err_no));
+			return res;
+		}
+		for (auto &iovec : writes) {
+			free((char *)iovec.file.path);
+			free(iovec.data);
+		}
+		writes.clear();
+	}
 
-
-    if (writes.size() >= min_count) {
-        res = make_dirs(directories);
-        if (!tc_okay(res)) {
-            return res;
-        }
-        res = tc_writev(writes.data(), writes.size(), false);
-        if (!tc_okay(res)) {
-            printf("writev: %s\n", strerror(res.err_no));
-            return res;
-        }
-        for (auto& iovec : writes) {
-            free((char*)iovec.file.path);
-            free(iovec.data);
-        }
-        writes.clear();
-    }
-
-    return res;
+	return res;
 }
 
-int main(int argc, char **argv) {
-    char exe_path[PATH_MAX];
-    char tc_config_path[PATH_MAX];
-    void *context;
-    struct archive *a;
-    struct archive_entry *entry;
-    int r;
-    tc_res res;
-    struct tc_iovec *input_file = (tc_iovec*) malloc(sizeof(struct tc_iovec));
-    std::vector<struct tc_attrs> directories;
-    std::vector<struct tc_iovec> writes;
-    std::vector<const char *> symlink_src_paths;
-    std::vector<const char *> symlink_dst_paths;
+int main(int argc, char **argv)
+{
+	char exe_path[PATH_MAX];
+	char tc_config_path[PATH_MAX];
+	void *context;
+	struct archive *a;
+	struct archive_entry *entry;
+	int r;
+	tc_res res;
+	struct tc_iovec *input_file =
+	    (tc_iovec *)malloc(sizeof(struct tc_iovec));
+	std::vector<struct tc_attrs> directories;
+	std::vector<struct tc_iovec> writes;
+	std::vector<const char *> symlink_src_paths;
+	std::vector<const char *> symlink_dst_paths;
 
-    readlink("/proc/self/exe", exe_path, PATH_MAX);
-    snprintf(tc_config_path, PATH_MAX,
-                 "%s/../../../../config/tc.ganesha.conf", dirname(exe_path));
-    fprintf (stderr, "using config file: %s\n", tc_config_path);
+	readlink("/proc/self/exe", exe_path, PATH_MAX);
+	snprintf(tc_config_path, PATH_MAX,
+		 "%s/../../../../config/tc.ganesha.conf", dirname(exe_path));
+	fprintf(stderr, "using config file: %s\n", tc_config_path);
 
-    context = tc_init (tc_config_path, DEFAULT_LOG_FILE, 77);
-    if (!context)
-    {
-        printf("initializing tc failed\n");
-        return 1;
-    }
-    if (on_exit(deinit, (void*)context) != 0) {
-        fprintf(stderr, "warning: failed to register tc_deinit() on exit");
-    }
+	context = tc_init(tc_config_path, DEFAULT_LOG_FILE, 77);
+	if (!context) {
+		printf("initializing tc failed\n");
+		return 1;
+	}
+	if (on_exit(deinit, (void *)context) != 0) {
+		fprintf(stderr,
+			"warning: failed to register tc_deinit() on exit");
+	}
 
-    const int read_size = 1024*1024;
-    input_file->file = tc_file_from_path(argv[1]);
-    input_file->offset = 0;
-    input_file->length = read_size;
-    input_file->data = (char*) malloc(read_size);
+	const int read_size = 1024 * 1024;
+	input_file->file = tc_file_from_path(argv[1]);
+	input_file->offset = 0;
+	input_file->length = read_size;
+	input_file->data = (char *)malloc(read_size);
 
-    a = archive_read_new();
-    archive_read_support_filter_all(a);
-    archive_read_support_format_all(a);
-    r = archive_read_open(a, input_file, NULL, tc_archive_read, NULL);
-    if (r != ARCHIVE_OK) {
-        printf("error, could not open archive");
-        return 1;
-    }
-    while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
-        mode_t type = archive_entry_filetype(entry);
-        if (S_ISDIR(type)) {
-            struct tc_attrs dir;
-            dir.file = tc_file_from_path(strdup(archive_entry_pathname(entry)));
-            dir.masks = TC_ATTRS_MASK_NONE;
-            dir.masks.has_mode = true;
-            dir.mode = 0755;
-            directories.push_back(dir);
-        } else if (S_ISREG(type)) {
-            size_t size = archive_entry_size(entry);
-            void *buff = malloc(size);
-            struct tc_iovec iovec;
+	a = archive_read_new();
+	archive_read_support_filter_all(a);
+	archive_read_support_format_all(a);
+	r = archive_read_open(a, input_file, NULL, tc_archive_read, NULL);
+	if (r != ARCHIVE_OK) {
+		printf("error, could not open archive");
+		return 1;
+	}
+	while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
+		mode_t type = archive_entry_filetype(entry);
+		if (S_ISDIR(type)) {
+			struct tc_attrs dir;
+			dir.file = tc_file_from_path(
+			    strdup(archive_entry_pathname(entry)));
+			dir.masks = TC_ATTRS_MASK_NONE;
+			dir.masks.has_mode = true;
+			dir.mode = 0755;
+			directories.push_back(dir);
+		} else if (S_ISREG(type)) {
+			size_t size = archive_entry_size(entry);
+			void *buff = malloc(size);
+			struct tc_iovec iovec;
 
-            r = archive_read_data(a, buff, size);
-            if (r != size) {
-                printf("error: r != size\n");
-            }
+			r = archive_read_data(a, buff, size);
+			if (r != size) {
+				printf("error: r != size\n");
+			}
 
-            iovec.file = tc_file_from_path(strdup(archive_entry_pathname(entry)));
-            iovec.is_creation = true;
-            iovec.offset = 0;
-            iovec.length = size;
-            iovec.data = (char*)buff;
-            writes.push_back(iovec);
-        } else if (S_ISLNK(type)) {
-            symlink_src_paths.push_back(strdup(archive_entry_symlink(entry)));
-            symlink_dst_paths.push_back(strdup(archive_entry_pathname(entry)));
-        } else {
-            printf("unhandled type: %s\n", archive_entry_pathname(entry));
-        }
+			iovec.file = tc_file_from_path(
+			    strdup(archive_entry_pathname(entry)));
+			iovec.is_creation = true;
+			iovec.offset = 0;
+			iovec.length = size;
+			iovec.data = (char *)buff;
+			writes.push_back(iovec);
+		} else if (S_ISLNK(type)) {
+			symlink_src_paths.push_back(
+			    strdup(archive_entry_symlink(entry)));
+			symlink_dst_paths.push_back(
+			    strdup(archive_entry_pathname(entry)));
+		} else {
+			printf("unhandled type: %s\n",
+			       archive_entry_pathname(entry));
+		}
 
-        res = performTCIO(directories, writes, symlink_src_paths, symlink_dst_paths, 255);
-        if (!tc_okay(res)) {
-            return res.err_no;
-        }
-    }
+		res = performTCIO(directories, writes, symlink_src_paths,
+				  symlink_dst_paths, 255);
+		if (!tc_okay(res)) {
+			return res.err_no;
+		}
+	}
 
-    res = performTCIO(directories, writes, symlink_src_paths, symlink_dst_paths, 0);
-    if (!tc_okay(res)) {
-        return res.err_no;
-    }
+	res = performTCIO(directories, writes, symlink_src_paths,
+			  symlink_dst_paths, 0);
+	if (!tc_okay(res)) {
+		return res.err_no;
+	}
 
-    r = archive_read_free(a);
-    if (r != ARCHIVE_OK) {
-        printf("error, could not free archive");
-        return 1;
-    }
+	r = archive_read_free(a);
+	if (r != ARCHIVE_OK) {
+		printf("error, could not free archive");
+		return 1;
+	}
 }
